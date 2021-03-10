c21ba0c862bfed70ef6d21faf29244664d051e75
Wildan Fauzy <me@wildanfauzy.com>
2021-03-11 03:27:11 +0700

    Initial commit

---
+++ src/content/kedinginan-di-bandung.md
@@ -0,0 +1,24 @@
+---
+layout: post
+title: Kedinginan di Bandung
+image: img/pexels-matheus-bertelli-799463_kx2qvc.jpg
+author: [Wildan Fauzy]
+date: 2021-02-19T10:00:00.000Z
+tags:
+  - catatan
+excerpt: Selimut sarung tidak mempan untuk menghalau rasa dingin, apalagi kalo setiap subuh datang.
+---
+
+Keputusan untuk kembali ke bandung cukup susah, cuaca di rumah sudah nyaman sekali, setiap hari hujan, hawa panas sudah tidak lagi menyelimuti hari-hari yang biasanya panas, kini sudah adem dan tentram.
+
+Namun kesendirian membuat jiwa yang kesepian ini semakin kacau, apalagi binatang yang bikin kesel selalu menemani setiap malam, sudah satu minggu yang lalu berniat untuk berangkat namun selalu saja ditunda besoknya lalu besoknya lagi.
+
+Sudah satu minggu berlalu baru deh akhirnya jadi berangkat ke kota kembang yang sekarang sedang mekar karena musim hujan, hari pertama tidak ada yang namanya mandi.
+
+Memasuki hari kedua baru akhirnya mencoba mandi, sial dinginnya bukan main, baru diguyur sekali langsung merinding dan gemeteran tidak mau berhenti disekujur badan, namun hal itu tidak berlangsung lama.
+
+Pada akhirnya mandi dengan mengelap air satu serokan tangan, tidak berani langsung mengguyur dengan gayung, setelah mandi badan memang menjadi anget namun rasa dingin langsung merasuk ke tulang.
+
+Selimut sarung tidak mempan untuk menghalau rasa dingin, apalagi kalo setiap subuh datang rasanya seperti masuk di dalam magic jar berisi preon ac, panas dingin tidak karuan.
+
+Sepertinya cukup sekali saja mandi, atau menunggu matahari bersinar terang dan membuat badan berkeringat lalu baru mandi, entah lah hanya alam dan seisinya yang tahu.rt your own blog with the same theme.
---
+++ src/website-config.ts
@@ -0,0 +1,68 @@
+export interface WebsiteConfig {
+  title: string;
+  description: string;
+  coverImage?: string;
+  logo: string;
+  /**
+   * Specifying a valid BCP 47 language helps screen readers announce text properly.
+   * See: https://dequeuniversity.com/rules/axe/2.2/valid-lang
+   */
+  lang: string;
+  /**
+   * blog full path, no ending slash!
+   */
+  siteUrl: string;
+  /**
+   * full url, no username
+   */
+  facebook?: string;
+  /**
+   * full url, no username
+   */
+  twitter?: string;
+  /**
+   * hide or show all email subscribe boxes
+   */
+  showSubscribe: boolean;
+  /**
+   * create a list on mailchimp and then create an embeddable signup form. this is the form action
+   */
+  mailchimpAction?: string;
+  /**
+   * this is the hidden input field name
+   */
+  mailchimpName?: string;
+  /**
+   * name and id of the mailchimp email field
+   */
+  mailchimpEmailFieldName?: string;
+  /**
+  /**
+   * Meta tag for Google Webmaster Tools
+   */
+  googleSiteVerification?: string;
+  /**
+  /**
+   * Appears alongside the footer, after the credits
+   */
+  footer?: string;
+}
+
+const config: WebsiteConfig = {
+  title: 'Aidan Blog',
+  description: 'Stories That Matters',
+  coverImage: 'img/blog-cover.png',
+  logo: 'img/ghost-logo.png',
+  lang: 'id',
+  siteUrl: 'https://www.aidanblog.com',
+  facebook: 'https://www.facebook.com/wilfauzy',
+  twitter: 'https://twitter.com/wilfauzy',
+  showSubscribe: true,
+  mailchimpAction: 'https://wildanfauzy.us10.list-manage.com/subscribe/post?u=72d29580a7ee388212e8622a5&id=f06aca7400',
+  mailchimpName: 'u=72d29580a7ee388212e8622a5&id=f06aca7400',
+  mailchimpEmailFieldName: 'MERGE0',
+  googleSiteVerification: 'GoogleCode',
+  footer: 'Member of NgopiBrek!',
+};
+
+export default config;
