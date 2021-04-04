eaf4130dc179d2d17dcea78a64aab58075a32175
Wildan Fauzy <me@wildanfauzy.com>
2021-04-04 14:53:44 +0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,5 @@
+// In your gatsby-config.js
+plugins: [`gatsby-plugin-gatsby-cloud`]
 /* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path');
 
@@ -17,18 +19,6 @@ module.exports = {
       options: {
         defaultQuality: 100,
         stripMetadata: true,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-gatsby-cloud`,
-      options: {
-        headers: {}, // option to add more headers. `Link` headers are transformed by the below criteria
-        allPageHeaders: [], // option to add headers for all pages. `Link` headers are transformed by the below criteria
-        mergeSecurityHeaders: true, // boolean to turn off the default security headers
-        mergeLinkHeaders: true, // boolean to turn off the default gatsby js headers
-        mergeCachingHeaders: true, // boolean to turn off the default caching headers
-        transformHeaders: (headers, path) => headers, // optional transform for manipulating headers under each path (e.g.sorting), etc.
-        generateMatchPathRewrites: true, // boolean to turn off automatic creation of redirect rules for client only paths
       },
     },
     {
