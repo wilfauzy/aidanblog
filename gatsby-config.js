17fd4b53ef147004295071a99bf5f9a3d41ff21c
wildan fauzy <me@wildanfauzy.com>
2021-04-05 05:14:51 +0700

    DDDJIE

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,6 +20,9 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-plugin-gatsby-cloud`,
+    },
+    {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'content',
